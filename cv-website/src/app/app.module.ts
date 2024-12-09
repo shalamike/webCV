75e68c1e892891eab66eb39b1c1dd5aa587f30d6
shalamike <m.shalaby1994@gmail.com>
2024-12-09 18:48:06 +0000

    added component for world countries to pull data from an API that will be created. Modified Profile Component to incorporate a table that will serve as the foundation for building a table in world countries

--- cv-website/src/app/app.module.ts
+++ cv-website/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
+import { WorldPopulationsComponent } from './world-populations/world-populations.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent
+    ProfileComponent,
+    WorldPopulationsComponent
   ],
   imports: [
     BrowserModule,
---
+++ cv-website/src/app/world-populations/world-populations.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-world-populations',
+  templateUrl: './world-populations.component.html',
+  styleUrls: ['./world-populations.component.css']
+})
+export class WorldPopulationsComponent {
+
+}
