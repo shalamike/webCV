a1cbf5c381c957e3cbe5383dad7c876a63c81c44
shalamike <m.shalaby1994@gmail.com>
2024-12-10 00:10:49 +0000

    seperated methods for fetching from api, added a table for different sprites

---
+++ cv-website/src/app/pokemon/pokemon.component.ts
@@ -0,0 +1,66 @@
+import { Component } from '@angular/core';
+import { PokemonService } from '../services/pokemon.service';
+
+@Component({
+  selector: 'app-pokemon',
+  templateUrl: './pokemon.component.html',
+  styleUrls: ['./pokemon.component.css'],
+})
+export class PokemonComponent {
+  searchName: string = '';
+  pokemonData: any = null;
+  errorMessage: string = '';
+  abilityDescriptions: { [key: string]: string } = {}; // Store ability descriptions
+
+  constructor(private pokemonService: PokemonService) {}
+
+  fetchPokemon(): void {
+    if (!this.searchName) {
+      this.errorMessage = 'Please enter a Pokémon name.';
+      this.pokemonData = null;
+      return;
+    }
+  
+    this.getPokemonData(this.searchName.toLowerCase());
+  }
+  
+  private getPokemonData(name: string): void {
+    this.pokemonService.getPokemonByName(name).subscribe({
+      next: (data) => {
+        this.pokemonData = data;
+        this.errorMessage = '';
+        this.fetchAbilitiesDescriptions(this.pokemonData.abilities);
+      },
+      error: () => {
+        this.errorMessage = 'Pokémon not found!';
+        this.pokemonData = null;
+      },
+    });
+  }
+  
+  private fetchAbilitiesDescriptions(abilities: any[]): void {
+    abilities.forEach((ability: any) => {
+      this.getAbilityDescription(ability.ability.name, ability.ability.url);
+    });
+  }
+  
+  private getAbilityDescription(name: string, url: string): void {
+    this.pokemonService.getPokemonAbilitiesDetails(url).subscribe({
+      next: (abilityData) => {
+        const englishEffect = this.extractEnglishEffect(abilityData);
+        this.abilityDescriptions[name] = englishEffect || 'Description not available.';
+      },
+      error: () => {
+        this.abilityDescriptions[name] = 'Description not available.';
+      },
+    });
+  }
+  
+  private extractEnglishEffect(abilityData: any): string | null {
+    const effectEntry = abilityData.effect_entries.find(
+      (entry: any) => entry.language.name === 'en'
+    );
+    return effectEntry?.effect || null;
+  }
+  
+}
---
+++ cv-website/src/app/services/pokemon.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokemonService {
+  private pokemonUrl = 'https://pokeapi.co/api/v2/pokemon/';
+
+  constructor(private http: HttpClient) {}
+
+  getPokemonByName(name: string): Observable<any> {
+    return this.http.get<any>(`${this.pokemonUrl}${name}`);
+  }
+
+  getPokemonAbilitiesDetails(url: string): Observable<any> {
+    return this.http.get<any>(url)
+  }
+
+
+}
