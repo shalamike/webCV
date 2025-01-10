22f99673e56c1d49121fb4535a2e2b4dba856520
Mike <m.shalaby1994@gmail.com>
2025-01-10 06:06:12 +0000

    reworkded the pokemon component completely so that it doesnt interfere with the parents components structure

--- cv-website/src/app/app-routing.module.ts
+++ cv-website/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HobbiesInterestsProjectsComponent } from './hobbies-interests-projects/hobbies-interests-projects.component';
+import { PokemonComponent } from './pokemon/pokemon.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HobbiesInterestsProjectsComponent },
+  { path: 'pokemon', component: PokemonComponent }, // New Pokémon route
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
