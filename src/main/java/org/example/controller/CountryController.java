e0b4a4061e0c88f92dc03bc426068af86894b02e
Mike <m.shalaby1994@gmail.com>
2025-01-15 10:15:31 +0000

    modified the country controller to return the total urban population of a given country which is the sum of all the city populaitons

--- src/main/java/org/example/controller/CountryController.java
+++ src/main/java/org/example/controller/CountryController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import static org.example.controller.ResponseHandler.handleResponse;
@@ -18,6 +20,8 @@ public class CountryController {
     @Autowired
     private CountryService countryService;
 
+
+
     private String objectType = "country";
     @GetMapping
     public ResponseEntity<?> getAllCountries(){
@@ -26,7 +30,22 @@ public class CountryController {
 
     @GetMapping("/country")
     public ResponseEntity<?> findCountryByName(@RequestParam String name){
-        return handleResponse(countryService.findCountryByName(name), objectType);
+//        return handleResponse(countryService.findCountryByName(name), objectType);
+        try{
+            Optional<?> citiesByCountryName = countryService.findCountryByName(name);
+            Optional<?> countriesPopulation = countryService.getUrbanPopulationOfCountry(name);
+            if (citiesByCountryName.isPresent() && countriesPopulation.isPresent()){
+                Map<String, Object> response = new HashMap<>();
+                response.put("totalUrbanPopulation",countriesPopulation.get());
+                response.put("country", citiesByCountryName.get());
+                return ResponseEntity.ok(response); //return a 200 response returns the countries information and the list of cities within the country
+            }
+            else {
+                return ResponseEntity.status(404).body(objectType + " not found with that criteria");
+            }
+        }catch (Exception e){
+            return ResponseEntity.status(500).body("An error occurred while fetching " + objectType + " " + e.getMessage());
+        }
     }
 
     @GetMapping("/language")
--- src/main/java/org/example/service/CountryService.java
+++ src/main/java/org/example/service/CountryService.java
@@ -1,12 +1,15 @@
 package org.example.service;
 
 
+import org.example.model.entities.City;
 import org.example.model.entities.Country;
+import org.example.model.repositories.CityRepository;
 import org.example.model.repositories.CountryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 import static org.example.service.HandleErrors.handleErrorsOrReturn;
@@ -16,6 +19,9 @@ public class CountryService {
     private String objectType = "country";
     @Autowired
     private CountryRepository countryRepository;
+
+    @Autowired
+    private CityRepository cityRepository;
 
     public Optional<?> findCountriesByContinent(String continent){
         return handleErrorsOrReturn(countryRepository.findByContinent(continent), objectType);
@@ -36,5 +42,18 @@ public class CountryService {
 
     public Optional<?> findCountriesWhereLanguageIsOfficial(String language){
         return handleErrorsOrReturn(countryRepository.findCountriesWhereLanguageIsOfficial(language), objectType);
+    }
+
+    public Optional<?> getUrbanPopulationOfCountry(String name){
+        try{
+
+            return Optional.of(cityRepository.findByCountryName(name).get().
+                    stream().
+                    mapToLong(City::getPopulation)
+                    .sum());
+        } catch (Exception e){
+            System.err.println("Error fetching " + objectType + " " + e.getMessage());
+            return Optional.empty();
+        }
     }
 }
